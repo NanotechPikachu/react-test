92499a62e46c788cd71f119fb3392c0352ca9e4f
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-01-20 16:15:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -52,8 +52,8 @@ function Square({ value, onSqClick }) {
 
 function winner(square) {
   const l = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
-  for (let i = 0, i < l.length, i++) {
-    const [a,b,c] = l[i];
+  for (let k = 0, k < l.length, k++) {
+    const [a,b,c] = l[k];
     if (square[a] && square[a] === square[b] && square[a] === square[c]) return square[a];
   }
   return null;
