52e2129fe7720089b94598400ef3e05492a2250b
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-01-22 19:58:01 +0530

    Update Job.js

--- src/Job.js
+++ src/Job.js
@@ -41,7 +41,7 @@ function ProductCatRow({ category }) {
 
 function ProductTable({ products }) {
   const rows = [];
-  const lastCat = null;
+  let lastCat = null;
   
   products.forEach((p) => {
     if (p.category !== lastCat) {
