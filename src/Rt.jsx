44b547834000a4a4fadf4f2d08ae336f884a40cc
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-01-23 07:44:55 +0530

    Update src

---
+++ src/Rt.jsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from 'react-router-dom';
+import Tic from './App';
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Tic />} />
+    <Routes />
+  )
+}
