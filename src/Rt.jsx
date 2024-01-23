5f8ba0687b2762800fedcc0e2a5ab157cf17183b
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-01-23 07:54:52 +0530

    Update Rt.jsx

--- src/Rt.jsx
+++ src/Rt.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Tic from './App';
+import Sr from './Job';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Tic />} />
+      <Route path="/search" element={<Sr />} />
     </Routes>
   )
 }
