8d313fc06c97edb36ecc1274e3c883aad3e61a47
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-01-23 07:48:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
 import "./styles.css";
 
-import App from "./Job";
-//import App from "./App";
+//import App from "./Job";
+import App from "./Rt";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
