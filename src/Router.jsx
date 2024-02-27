bfd4207d7a2857f30c826ad21f0e271cf513fb69
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-27 14:17:50 +0530

    Update and rename Rt.jsx to Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Tic from './App';
-import Sr from './Job';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Tic />} />
-      <Route path="/search" element={<Sr />} />
     </Routes>
   )
 }
