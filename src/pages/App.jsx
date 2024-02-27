8c6c65190416e66893bc0851be9b13afe20e2761
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-27 16:43:21 +0530

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -10,7 +10,7 @@ export default function Page() {
 
 function NavBar() {
   return (
-   <nav className="fixed top-0 left-0 w-full flex justify-center items-center py-4 px-8 border-b border-primary-color shadow-md z-10 bg-black backdrop-filter backdrop-blur-lg opacity-50">
+   <nav className="fixed top-0 left-0 w-full flex py-4 px-8 border-b border-primary-color shadow-md z-10 bg-black backdrop-filter backdrop-blur-lg opacity-50">
     <p className="font-bold text-left text-sky-400 hover:text-amber-700 text-xl font-sans">NanotechWiki</p>
     </nav>
   )
