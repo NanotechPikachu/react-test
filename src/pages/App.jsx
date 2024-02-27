db6f78b6f151486bafc3eaab42fd85eb15369d92
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-27 16:03:16 +0530

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export default function Page() {
   return (
     <>
-    <div className="bg-gradient-to-r from-red-400/90" />
+    <div className="bg-gradient-to-r from-red-400/90 to-blue-400/40" />
     <h1 className="text-left text-sky-400 hover:text-amber-700 text-xl font-sans">Nanotech Wiki</h1>
     </>
   )
