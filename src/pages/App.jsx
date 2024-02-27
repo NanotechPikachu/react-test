374f6bae3c422dede9431cae65b9b9f4b1eaf9ac
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-27 15:01:26 +0530

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 export default function Page() {
   return (
-    <div className="text-amber-700/90">
+    <div className="bg-red-400/90">
     <h1 className="text-left text-sky-400 hover:text-amber-700 text-xl font-sans">Nanotech Wiki</h1>
     </div>
   )
