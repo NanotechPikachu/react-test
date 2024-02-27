97446c2b27f53f5f5a0884fa7dcd8a7a471095c8
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-27 16:17:11 +0530

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,9 +2,8 @@ import '../css/styles.css';
 
 export default function Page() {
   return (
-    <>
-    <div className="bg-gradient-to-r from-red-400/90 to-blue-400/40" />
+    <div>
     <h1 className="text-left text-sky-400 hover:text-amber-700 text-xl font-sans">Nanotech Wiki</h1>
-    </>
+    </div>
   )
 }
