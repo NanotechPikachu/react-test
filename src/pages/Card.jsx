b2e0651ced550ffb86ef45d94bf65b1138804876
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-28 18:14:09 +0530

    Update Card.jsx

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,8 +1,8 @@
 import '../css/styles.css';
 
-export default function Card({ title, children }) {
+export default function Card({ title, children, className }) {
   return (
-    <div className="border md:w-96 p-5 m-5 bg-zinc-900 backdrop-blur overflow-hidden">
+    <div className={`border md:w-96 p-5 m-5 bg-zinc-900 backdrop-blur overflow-hidden ${className}`}>
       <h2 className="text-2xl font-bold mb-4 text-white whitespace-pre-line">{title}</h2>
       <div className="whitespace-pre-line text-white">{children}</div>
     </div>
