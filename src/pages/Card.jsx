c189c673a1c44c0e3ba14073f489d8b82b1f139c
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-28 12:58:40 +0530

    Update Card.jsx

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -2,7 +2,7 @@ import '../css/styles.css';
 
 export default function Card({ title, children }) {
   return (
-    <div className="border p-5 m-5">
+    <div className="border p-5 m-5 bg-black bg-opacity-50 backdrop-blur">
       <h2 className="text-2xl font-bold mb-4">{title}</h2>
       {children}
     </div>
