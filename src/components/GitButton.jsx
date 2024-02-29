66402cce393391af6214a244fdef39630f9db4b4
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-29 11:49:44 +0530

    Make GitButton

---
+++ src/components/GitButton.jsx
@@ -0,0 +1,9 @@
+import '../css/styles.css';
+
+export default function GitButton() {
+  return (
+    <a href="https://github.com/NanotechPikachu/aoi-wikis/" target="_blank" rel="noopener noreferrer">
+    <button className="bg-transparent text-white mt-10 rounded-full text-center py-2 px-4 mr-10">GitHub repository</button>
+    </a>
+  )
+}
