3b41c97bdd8b34a7e98c5d49bd1ca630b8c9be73
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-29 12:16:51 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,9 +5,8 @@ export default function Card({ title, children, icon, className }) {
   return (
     <div className={`border md:w-96 p-5 m-5 bg-zinc-900 backdrop-blur overflow-hidden flex items-center ${className}`}>
       {icon && (
-        <svg className="text-blue-500 w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          {/* Replace with your actual SVG path */}
-          <path d="M0 25h40m-30-20l20 20-20 20" fill="none" stroke="blue" stroke-width="5"/>
+        <svg className="text-blue-500 w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+        <path d="M10 25h30m-20-15l15 15-15 15" fill="none" stroke="currentColor" strokeWidth="2"/>
         </svg>
       )}
       <h2 className={`text-2xl font-bold mb-4 text-white whitespace-pre-line ${icon ? 'flex-grow' : ''}`}>{title}</h2>
