4516bc0c47edadb23e3acdeb0f6b09aaf839f169
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-29 11:38:02 +0530

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,11 @@
 import '../css/styles.css';
-import Card from './Card';
+import Card from '../components/Card';
+import TitleNavBar from '../components/TitleNavBar';
 
 export default function Page() {
   return (
     <div>
-    <NavBar />
+    <TitleNavBar />
     <h1 className="pt-24 pb-4 text-center font-bold text-zinc-50 hover:text-red-50 text-2xl/[16px]">Nanotech Wiki</h1>
     <p className="text-center font-sans text-grey-50">A wiki for all by NanotechPikachu</p>
     <div className="flex justify-between px-4 items-center">
@@ -20,10 +21,4 @@ hwhwgahahahaba </p>
   )
 }
 
-function NavBar() {
-  return (
-   <nav className="fixed top-0 left-0 w-full flex py-4 px-8 border-b border-primary-color shadow-md z-10 backdrop-filter backdrop-blur-lg opacity-60">
-    <p className="font-bold text-left text-sky-400 hover:text-amber-700 text-xl font-sans">Nanotech Wiki</p>
-    </nav>
-  )
-}
+
