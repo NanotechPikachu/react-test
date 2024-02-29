ed545b83a67ed149d3d2a9d990b7b344e129ebf8
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-29 12:32:27 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ export default function Card({ title, children, icon, className }) {
     <div className={`border md:w-96 p-5 m-5 bg-zinc-900 backdrop-blur overflow-hidden flex flex-col items-start ${className}`}>
       <div className="flex items-center w-full">
         {icon && (
-          <svg className="text-blue-500 w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <svg className="text-blue-500 w-6 h-6 mr-2 align-baseline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path d="M17.92 11.62a1.001 1.001 0 0 0-.21-.33l-5-5a1.003 1.003 0 1 0-1.42 1.42l3.3 3.29H7a1 1 0 0 0 0 2h7.59l-3.3 3.29a1.002 1.002 0 0 0 .325 1.639 1 1 0 0 0 1.095-.219l5-5a1 1 0 0 0 .21-.33 1 1 0 0 0 0-.76Z" fill="currentColor"/>
           </svg>
         )}
