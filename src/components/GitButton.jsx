7e954e873fe874d6ceba5627bb84fe04803e9c1a
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-29 18:04:01 +0530

    Update GitButton.jsx

--- src/components/GitButton.jsx
+++ src/components/GitButton.jsx
@@ -2,7 +2,7 @@ import '../css/styles.css';
 
 export default function GitButton() {
   return (
-    <a href="https://github.com/NanotechPikachu/aoi-wikis/" target="_blank" rel="noopener noreferrer">
+    <a href="https://github.com/NanotechPikachu/anichu.db/" target="_blank" rel="noopener noreferrer">
     <button className="bg-transparent text-white mt-10 rounded-full text-center py-2 px-4 mr-10">GitHub repository</button>
     </a>
   )
