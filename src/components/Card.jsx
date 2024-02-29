24180913c2186ac1ef4856c581a115c7ef102c6e
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-29 12:11:55 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,16 @@
+import React from 'react';
 import '../css/styles.css';
 
-export default function Card({ title, children, className }) {
+export default function Card({ title, children, icon, className }) {
   return (
-    <div className={`border md:w-96 p-5 m-5 bg-zinc-900 backdrop-blur overflow-hidden ${className}`}>
-      <h2 className="text-2xl font-bold mb-4 text-white whitespace-pre-line">{title}</h2>
+    <div className={`border md:w-96 p-5 m-5 bg-zinc-900 backdrop-blur overflow-hidden flex items-center ${className}`}>
+      {icon && (
+        <svg className="text-blue-500 w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          {/* Replace with your actual SVG path */}
+          <path d="M0 25h40m-30-20l20 20-20 20" fill="none" stroke="blue" stroke-width="5"/>
+        </svg>
+      )}
+      <h2 className={`text-2xl font-bold mb-4 text-white whitespace-pre-line ${icon ? 'flex-grow' : ''}`}>{title}</h2>
       <div className="whitespace-pre-line text-white">{children}</div>
     </div>
   );
