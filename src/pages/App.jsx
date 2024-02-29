b3adb4a8d39c32dfe0d0926fbacb0e8f5db2408d
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-29 17:47:33 +0530

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -13,8 +13,8 @@ export default function Page() {
     <button className="bg-blue-500 hover:bg-blue-400 text-white mt-10 rounded-full ml-10 text-center py-2 px-4">Click Me</button>
     <GitButton />
     </div>
-    <h2 className="text-xl font-bold text-black mt-10 ml-10 text-left">Features</h2>
-    <Card title="AniChu.DB" className="mt-8" icon={true}>
+    <h2 className="text-xl font-bold text-white mt-10 ml-5 text-left">Features</h2>
+    <Card title="AniChu.DB" className="mt-5" icon={true}>
     <p> A DataBase of all animes. </p>
     </Card>
     <Card title="Tested" className="mt-4" icon={true}>
