daee3d3382e8cdef83844a8ffaaf98968507eadb
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-29 11:38:58 +0530

    Make title navbar

---
+++ src/components/TitleNavBar.jsx
@@ -0,0 +1,9 @@
+import '../css/styles.css';
+
+export default function TitleNavBar() {
+  return (
+   <nav className="fixed top-0 left-0 w-full flex py-4 px-8 border-b border-primary-color shadow-md z-10 backdrop-filter backdrop-blur-lg opacity-60">
+    <p className="font-bold text-left text-sky-400 hover:text-amber-700 text-xl font-sans">Nanotech Wiki</p>
+    </nav>
+  )
+}
