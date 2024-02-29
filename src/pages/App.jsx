4cb2dde1258892796aaa9d0bf085586434b3ed85
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-29 16:59:43 +0530

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -10,7 +10,7 @@ export default function Page() {
     <h1 className="pt-24 pb-4 text-center font-bold text-zinc-50 hover:text-red-50 text-2xl/[16px]">Nanotech Wiki</h1>
     <p className="text-center font-sans text-grey-50">A wiki for all by NanotechPikachu</p>
     <div className="flex justify-between px-4 items-center">
-    <button className="bg-blue-500 text-white mt-10 rounded-full ml-10 text-center py-2 px-4">Click Me</button>
+    <button className="bg-blue-500 hover:bg-blue-400 text-white mt-10 rounded-full ml-10 text-center py-2 px-4">Click Me</button>
     <GitButton />
     </div>
     <Card title="text" className="mt-10" icon={true}>
