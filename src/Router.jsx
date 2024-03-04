2ddb70501b83e57aaaecfed22794d63b822bd06a
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-04 20:32:23 +0530

    Update Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/App.jsx';
+import Start from './pages/Start.jsx';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/start" element={<Start />} />
     </Routes>
   )
 }
