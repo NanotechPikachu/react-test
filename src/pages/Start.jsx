4e0469df2870514163d6bd71dcd2923b7d8e7459
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-05 17:34:35 +0530

    Update Start.jsx

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,5 +1,9 @@
 export default function Start() {
   return (
-    <h1>Yo!</h1>
+    <div>
+    <h1 class="ml-4 text-2xl md:text-3xl">Get Started</h1>
+    <hr />
+    <h2 class="ml-4 text-xl">Installation</h2>
+    </div>
   )
 }
