6f6cf9ba5fe178e1f5133e5f686683cc31722559
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-05 19:23:07 +0530

    Update Start.jsx

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,10 +1,12 @@
+import Terminal from '../components/Terminal';
+
 export default function Start() {
   return (
     <div>
     <h1 class="ml-4 text-2xl md:text-3xl pt-24 mb-4 font-bold text-white">Get Started</h1>
     <hr />
     <h2 class="ml-4 text-xl mt-4 font-bold text-white">Installation</h2>
-    
+    <Terminal command="npm i anichu.db" />
     </div>
   )
 }
