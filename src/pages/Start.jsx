45a79bb1761ee0d24235a6745696095b7bf9e1ef
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-06 20:37:46 +0530

    Update Start.jsx

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -9,7 +9,7 @@ export default function Start() {
     <h2 className="ml-5 mr-5 text-xl mt-4 font-bold text-white">Installation</h2>
     <Terminal command="npm i anichu.db" />
     <h2 className="ml-5 mr-5 text-xl mt-6 font-bold text-white">Functions Available</h2>
-    <ul className="mt-3">
+    <ul className="mt-3 ml-5">
     <li key="func1"> <Code code="getAnime()" /> - To get an anime from it’s title. </li>
     </ul>
     </div>
