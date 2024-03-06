a37c48e4fd7b2ce0cf67a1e19e6695cf9ad1968d
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-06 07:00:20 +0530

    Update Terminal.jsx

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -5,15 +5,16 @@ import { ClipboardIcon } from '@heroicons/react/24/outline';
 export default function Terminal({ command }) {
   return (
     <>
-    <div className="flex justify-between items-center bg-black mt-4 ml-5 h-7 mr-5" />
-    <div className="flex justify-between items-center bg-zinc-900 text-green-400 p-3 ml-5 mr-5">
-    <span className="text-blue-500">$ {command}</span>
+    <div className="flex justify-between items-center bg-black mt-4 ml-5 h-7 mr-5">
     <button
           onClick={() => handleCopy(command)}
           className="bg-gray-700 hover:bg-gray-600 text-gray-400 rounded p-1"
         >
     <ClipboardIcon className="h-5 w-5" />
     </button>
+    </div>
+    <div className="flex justify-between items-center bg-zinc-900 text-green-400 p-3 ml-5 mr-5">
+    <span className="text-blue-500">$ {command}</span>
     </div>
     </>
   )
