f777f5015ca1fe1b6a06b40cd48e945f6e500235
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-06 07:40:40 +0530

    Update Start.jsx

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -3,10 +3,11 @@ import Terminal from '../components/Terminal';
 export default function Start() {
   return (
     <div>
-    <h1 class="ml-4 text-2xl md:text-3xl pt-24 mb-4 font-bold text-white">Get Started</h1>
+    <h1 className="ml-4 text-2xl md:text-3xl pt-24 mb-4 font-bold text-white">Get Started</h1>
     <hr />
-    <h2 class="ml-5 mr-5 text-xl mt-4 font-bold text-white">Installation</h2>
+    <h2 className="ml-5 mr-5 text-xl mt-4 font-bold text-white">Installation</h2>
     <Terminal command="npm i anichu.db" />
+    <h2 className="ml-5 mr-5 text-xl mt-6 font-bold text-white">Functions Available</h2>
     </div>
   )
 }
