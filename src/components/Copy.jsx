505d4a1fa6e280ef4117ceb41da4b54585759a5e
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-06 07:37:42 +0530

    Update Copy.jsx

--- src/components/Copy.jsx
+++ src/components/Copy.jsx
@@ -1,9 +1,13 @@
+import React from 'react';
 import { ClipboardIcon } from '@heroicons/react/24/outline';
 
 export default function Copy({ command }) {
   return (
     <>
-    <button>
+    <button
+      onClick={() => handleCopy(command)}
+      className="text-gray-400 rounded p-1 mr-3 border hover:bg-zinc-950"
+      >
     <ClipboardIcon className="h-4 w-4" />
     </button>
     </>
