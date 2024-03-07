efa82cbea6aced794cc022a877db1990437dde13
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-07 17:14:24 +0530

    Update PageCards.jsx

--- src/components/PageCards.jsx
+++ src/components/PageCards.jsx
@@ -4,15 +4,18 @@ import { useNavigate } from 'react-router-dom';
 export function NextCard({ link, title }) {
   return (
     <div className="mt-20 rounded-md text-right ml-5 mr-5 w-full border-2 border-slate-200/50 bg-transparent">
-    <div className="my-3">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
-    </svg>
-    <p className="text-sm text-slate-500/50">Next</p>
-    <h2 className="text-xl text-white">
-    {title}
-    </h2>
+      <div className="flex justify-end items-center">
+      <div className="flex flex-col mr-4">
+      <p className="text-sm text-slate-500/50">Next</p>
+      <h2 className="text-xl text-white mt-1">
+      {title}
+      </h2>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+      </svg>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
+
