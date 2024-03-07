fda6d2e96350e0fc855ae755829686dae87903bf
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-07 16:48:29 +0530

    Update PageCards.jsx

--- src/components/PageCards.jsx
+++ src/components/PageCards.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function NextCard({ link }) {
   return (
-    <div> 
+    <div className="mt-20 rounded-md text-left ml-5 mr-5 w-full border-2 border-slate-200/50"> 
     Yo
     </div>
   )
