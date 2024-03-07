b7d1bf18229ad3b64dc562cda4942b717af189cf
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-07 17:00:20 +0530

    Update PageCards.jsx

--- src/components/PageCards.jsx
+++ src/components/PageCards.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export function NextCard({ link }) {
+export function NextCard({ link, title }) {
   return (
-    <div className="mt-20 rounded-md text-left ml-5 mr-5 w-full border-2 border-slate-200/50"> 
-    Yo
+    <div className="mt-20 rounded-md text-left ml-5 mr-5 w-full border-2 border-slate-200/50 bg-transparent">
+    <div className="my-3"> 
+    <p className="text-sm text-slate-500/50">Next</p>
+    <h2 className="text-xl text-white">
+    {title}
+    </h2>
+    </div>
     </div>
   )
 }
