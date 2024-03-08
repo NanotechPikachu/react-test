fae5bd009858e354011dcb0527d2222af8f4348d
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-08 18:10:48 +0530

    Update PageCards.jsx

--- src/components/PageCards.jsx
+++ src/components/PageCards.jsx
@@ -34,7 +34,7 @@ export function PrevCard({ link, title, className }) {
 
   return (
     <div className={`mr-5 ml-5 rounded-md text-left border-2 border-slate-400/50 hover:border-lime-300/40 bg-transparent hover:text-lime-300/50 ${className}`} onClick={() => nextPage()}>
-      <div className="flex justify-start items-center pt-4 pb-4 pr-4 hover:text-lime-300/50">
+      <div className="flex justify-start items-center pt-4 pb-4 pr-4 pl-4 hover:text-lime-300/50">
         {/* SVG aligned to the left */}
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-4">
           <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
