ee6fb2b97cf3fa6be285e6167bd67cf221ce4694
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-08 08:51:12 +0530

    Update GitButton.jsx

--- src/components/GitButton.jsx
+++ src/components/GitButton.jsx
@@ -3,7 +3,7 @@ import '../css/styles.css';
 export default function GitButton() {
   return (
     <a href="https://github.com/NanotechPikachu/anichu.db/" target="_blank" rel="noopener noreferrer">
-    <button className="bg-transparent text-white mt-10 rounded-full text-center py-2 px-4">GitHub repository</button>
+    <button className="bg-transparent text-black mt-10 rounded-full text-center py-2 px-4">GitHub repository</button>
     </a>
   )
 }
