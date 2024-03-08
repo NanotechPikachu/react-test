11e0574986176ae861a720c044f5a49fd680f900
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-08 08:49:19 +0530

    Update Code.jsx

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Code({ code }) {
   return (
-    <span className="backdrop-blur text-white bg-zinc-300/50 p-0.5 font-mono text-sm">
+    <span className="backdrop-blur text-black bg-zinc-300/50 p-0.5 font-mono text-sm">
     {code}
     </span>
   )
