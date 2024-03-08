4088a0b5e2e979b3984d2fc5131da92ca2e5b290
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-08 08:54:44 +0530

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -15,7 +15,7 @@ export default function Page() {
       <TitleNavBar />
       <div className="flex flex-col md:pl-10">
         <div className="pt-24 pb-4 text-center md:ml-4 md:text-left">
-          <h1 className="font-bold text-zinc-800 hover:text-red-100 text-3xl md:text-4xl">AniChu.DB</h1>
+          <h1 className="font-bold text-zinc-800 hover:text-zinc-700 text-3xl md:text-4xl">AniChu.DB</h1>
           <p className="font-sans text-grey-50 md:text-xl">A JSON DataBase of anime info!</p>
         </div>
         <div className="flex md:justify-start md:items-center md:w-auto ml-6">
