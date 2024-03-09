4cc7bb920fc3080b5dbef0a91ba0e5b52bb20e39
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-09 16:16:29 +0530

    Update TitleNavBar.jsx

--- src/components/TitleNavBar.jsx
+++ src/components/TitleNavBar.jsx
@@ -2,7 +2,7 @@ import "../css/styles.css";
 
 export default function TitleNavBar() {
   return (
-    <div className="fixed top-0 left-0 w-full flex items-center justify-between py-4 px-8 border-b border-primary-color shadow-md z-10 nav">>
+    <div className="fixed top-0 left-0 w-full flex items-center justify-between py-4 px-8 border-b border-primary-color shadow-md z-10 nav">
         <p className="font-bold text-left text-sky-600 hover:text-amber-700 text-xl font-sans">
           Nanotech Wiki
         </p>
