32582118370f60b44e6ecd5b5fc8bdd3fc5bd812
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-09 10:22:44 +0530

    Update TitleNavBar.jsx

--- src/components/TitleNavBar.jsx
+++ src/components/TitleNavBar.jsx
@@ -2,8 +2,8 @@ import "../css/styles.css";
 
 export default function TitleNavBar() {
   return (
-    <div className="flex backdrop-blur-lg">
-      <nav className="backdrop-blur-lg fixed top-0 left-0 w-full flex items-center justify-between py-4 px-8 border-b border-primary-color shadow-md z-10 bg-white/30">
+    <div style={{ backdropFilter: 'blur(12px)' }}>
+      <nav className="fixed top-0 left-0 w-full flex items-center justify-between py-4 px-8 border-b border-primary-color shadow-md z-10 bg-white/30">
         <p className="font-bold text-left text-sky-600 hover:text-amber-700 text-xl font-sans">
           Nanotech Wiki
         </p>
