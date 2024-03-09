9e843309c4f9958d0a9703e840701207ecc86a12
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-09 08:50:14 +0530

    Update TitleNavBar.jsx

--- src/components/TitleNavBar.jsx
+++ src/components/TitleNavBar.jsx
@@ -2,10 +2,12 @@ import "../css/styles.css";
 
 export default function TitleNavBar() {
   return (
-    <nav className="fixed top-0 left-0 w-full flex py-4 px-8 border-b border-primary-color shadow-md z-10 backdrop-filter backdrop-blur-2xl opacity-60">
+    <div className="backdrop-blur-lg">
+    <nav className="fixed top-0 left-0 w-full flex py-4 px-8 border-b border-primary-color shadow-md z-10 backdrop-filter opacity-60">
       <p className="font-bold text-left text-sky-600 hover:text-amber-700 text-xl font-sans">
         Nanotech Wiki
       </p>
     </nav>
+    </div>
   );
 }
