1915ddd981237ec5dacab0b4691231b8d988ee71
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-09 07:54:02 +0530

    Make Result.jsx

---
+++ src/pages/Result.jsx
@@ -0,0 +1,13 @@
+import Code from '../components/Code';
+import { NextCard, PrevCard } from '../components/PageCards';
+
+export default function Result() {
+  return (
+    <div>
+    <h1 className="ml-4 text-3xl md:text-4xl pt-24 mb-4 font-bold text-black">Result Type</h1>
+    <hr />
+    <p className="ml-5 mr-5 mt-4 text-black">
+    This page deals with the JSON DB output as well as discussing the results or outputs of functions.
+    </p>
+  )
+}
