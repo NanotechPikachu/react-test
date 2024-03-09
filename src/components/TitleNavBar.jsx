ffd55d7ad5ecef0f03555351066d99ab5f8c9e4e
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-09 19:39:07 +0530

    Update TitleNavBar.jsx

--- src/components/TitleNavBar.jsx
+++ src/components/TitleNavBar.jsx
@@ -1,13 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import "../css/styles.css";
 
 export default function TitleNavBar() {
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    // Detect scroll position
+    const currentScrollPos = window.pageYOffset;
+    // Set state based on scroll direction
+    setVisible((prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 70) || currentScrollPos < 10);
+    // Save the new position for comparison
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos, visible]);
+
   return (
     <>
-    <nav className="top-0 left-0 w-full flex items-center justify-between py-4 px-8 border-b border-primary-color shadow-md z-10">
-    <p className="font-bold text-left text-sky-600 hover:text-amber-700 text-xl font-sans">
-    Nanotech Wiki
-    </p>
-    </nav>
+      <nav className={`fixed top-0 left-0 w-full flex items-center justify-between py-4 px-8 border-b border-primary-color shadow-md z-10 transition-all duration-300 ease-in-out ${visible ? 'bg-transparent' : 'bg-white shadow-lg'}`}>
+        <p className="font-bold text-left text-sky-600 hover:text-amber-700 text-xl font-sans">
+          Nanotech Wiki
+        </p>
+      </nav>
     </>
   );
 }
+
