f785b5e0f5ba4480d875d815be6c6d8300039a4b
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-10 09:29:28 +0530

    Update Result.jsx

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -9,6 +9,12 @@ export default function Result() {
     <p className="ml-5 mr-5 mt-4 text-black">
     This page deals with the JSON DB output as well as discussing the results or outputs of functions.
     </p>
+    <h2 className="ml-4 text-2xl md:text-3xl mt-8 font-bold text-black">getAnime()</h2>
+    <p className="ml-4 text-black mt-3">
+This function returns an Promise as such it’s an Asynchronous function needing to be <Code code="await" />-ed.
+
+The promise consists of an Object which has the relevant data of the anime(if any).
+    </p>
     </div>
   )
 }
