1d471611a0635b6c662b1ff6d0fa45f368713159
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-10 09:37:58 +0530

    Update Result.jsx

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -12,10 +12,10 @@ export default function Result() {
     <h2 className="ml-4 text-2xl md:text-3xl mt-8 font-bold text-black">getAnime()</h2>
     <p className="ml-4 text-black mt-3">
 This function returns an <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise" className="text-blue-600 hover:text-blue-700"><u>Promise</u></a> as such it’s an <b>Asynchronous</b> function needing to be <Code code="await" />-ed.
-<br />
+<br /> <br />
 The promise consists of an <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object" className="text-blue-600 hover:text-blue-700"><u>Object</u></a> which has the relevant data of the anime(if any).
     </p>
-    <h3 className="text-xl ml-4 mt-8 font-bold>
+    <h3 className="text-xl ml-4 mt-8 font-bold">
     Data Structure of <Code code="getAnime()" />
     </h3>
     </div>
