048956a7128bbd7244b9bead8ddfaf55c0ea0a7c
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-10 09:55:53 +0530

    Update CodeBlock.jsx

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -1,12 +1,10 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-/*
-export default function CodeBlock ({ 
-  const codeString = '(num) => num + 1';
+
+export default function CodeBlock({ lang, children }) {
   return (
-    <SyntaxHighlighter language="javascript" style={dark}>
-      {codeString}
+    <SyntaxHighlighter language={lang} style={dark}>
+      {children}
     </SyntaxHighlighter>
-  );
-};
-*/
+  )
+}
